a9ec21a53f310e893b55c588da525cde1527abcb
Valerie Oakhu <ooakhu@gmail.com>
2023-02-16 20:50:46 -0800

    low level programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(long int));
+printf(sizeof(long long int));
+printf(sizeof(float));
+return (0);
+}
