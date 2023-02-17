7b71c5e9a4fffb5d44bf14da7b05fa997b6eecd2
Valerie Oakhu <ooakhu@gmail.com>
2023-02-17 14:07:51 -0800

    low level

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
+return (0);
+}
+
