e26673431c61db0cd1af1280209fa76fae9d0aeb
Valerie Oakhu <ooakhu@gmail.com>
2023-02-17 14:44:08 -0800

    low level

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char d;
+
+for (d = 'z'; d >= 'a'; d--)
+putchar(d);
+putchar('\n');
+return (0);
+}
+
