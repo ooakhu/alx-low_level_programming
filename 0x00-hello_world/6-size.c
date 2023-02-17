e78251a2e48b3cef42f5c95fd6e92f9fcda4d01c
Valerie Oakhu <ooakhu@gmail.com>
2023-02-17 02:07:12 -0800

    low level corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
+int m;
+long int n;
+long long int b;
+char c;
+float v;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(m));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(v));
 return (0);
 }
