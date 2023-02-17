674c6f4c35801700e3a77f9a715ad7fca3363c05
Valerie Oakhu <ooakhu@gmail.com>
2023-02-17 06:14:27 -0800

    low level corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -13,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d is positve\n", n)
+printf("%d is positve\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", n)
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative\n", n)
+printf("%d is negative\n", n);
 }
 
 }
