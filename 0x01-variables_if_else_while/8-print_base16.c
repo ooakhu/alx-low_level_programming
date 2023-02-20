8027547042b7751ac0812d32791ca02913888089
Valerie Oakhu <ooakhu@gmail.com>
2023-02-20 11:29:40 -0800

    advanced

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -14,15 +13,10 @@ int num;
 char alpha;
 
 for (num = '0'; num <= '9'; num++)
-{
-putchar(num)
-}
+putchar(num);
+for (alpha = 'a'; alpha <= 'f'; alpha++)
+putchar(alpha);
+putchar('\n');
 
-for (alpha = 'a'; alpha <= 'b'; alpha++)
-{
-putchar(alpha)
-}
-putchar('\n')
-
-return (0)
+return (0);
 }
