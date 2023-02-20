4cf3e7ff3de4de96a6c24c8a42a1f55b2b15b48d
Valerie Oakhu <ooakhu@gmail.com>
2023-02-20 11:21:15 -0800

    advanced

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+char alpha;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num)
+}
+
+for (alpha = 'a'; alpha <= 'b'; alpha++)
+{
+putchar(alpha)
+}
+putchar('\n')
+
+return (0)
+}
