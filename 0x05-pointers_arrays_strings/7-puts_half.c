b2b52cd69345ab5e2e050d146ffe74c8bc708835
Valerie Oakhu <ooakhu@gmail.com>
2023-02-28 12:50:16 -0800

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * puts_half - a function that prints half of a string
  * @str: an input string
@@ -10,14 +12,14 @@ int len = 0, v, g;
 while (str[len] != '\0')
 len++;
 
-f (len % 2 == 0)
+ff (len % 2 == 0)
 g = len / 2;
 
 else
 g = (len + 1) / 2;
 
 for (v = g; v < len; v++)
-_putchar(str[v]);
+putchar(str[v]);
 
-_putchar('\n');
+putchar('\n');
 }
